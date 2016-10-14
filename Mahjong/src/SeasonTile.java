a5d06f91a3bd913d807e60aa80918d7c0b3d3201
Curtis Nixon <cnixon@axis41.com>
2016-10-14 15:43:51 -0600

    Add the images and work on SeasonTile's

--- Mahjong/src/SeasonTile.java
+++ Mahjong/src/SeasonTile.java
@@ -1,3 +1,9 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
 /**
  * Created by Curtis on 9/28/2016.
  */
@@ -6,4 +12,50 @@ public class SeasonTile extends PictureTile {
         super(name);
     }
 
+    // valid seasons are Spring Summer Fall Winter
+    //TODO make sure images are located in the right place for testing
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        String name = super.toString();
+        ImageIcon icon;
+        if(name.equals("Spring")){
+            icon = new ImageIcon("images/Spring.png");
+        }else if(name.equals("Summer")){
+            icon = new ImageIcon("images/Summer.png");
+        }else if(name.equals("Fall")){
+            icon = new ImageIcon("images/Fall.png");
+        }else if(name.equals("Winter")){
+            icon = new ImageIcon("images/Winter.png");
+        }else{
+            icon = new ImageIcon();
+        }
+        //TODO resize the images
+        Image image = icon.getImage();
+        image.getScaledInstance(10,10,Image.SCALE_DEFAULT);
+        g.drawImage(image, 20, 0, null);
+        setToolTipText(super.toString());
+
+    }
+
+
+    public static void main(String[] args)
+    {
+
+        JFrame	frame = new JFrame();
+
+        frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Season Tiles");
+
+        frame.add(new SeasonTile("Spring"));
+        frame.add(new SeasonTile("Summer"));
+        frame.add(new SeasonTile("Fall"));
+        frame.add(new SeasonTile("Winter"));
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
