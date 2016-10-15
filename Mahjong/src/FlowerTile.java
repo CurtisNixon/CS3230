8742f3bdca85afdc295e19308cb2c7d02209cff3
Curtis <curtisbnixon@gmail.com>
2016-10-15 13:53:25 -0600

    Add FlowerTile's

--- Mahjong/src/FlowerTile.java
+++ Mahjong/src/FlowerTile.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Curtis on 9/28/2016.
  */
@@ -6,4 +9,46 @@ public class FlowerTile extends PictureTile {
         super(name);
     }
 
+    public void paintFlowerTile(Graphics g){
+        String name = super.toString();
+        ImageIcon icon = null;
+        if(name.equals("Chrysanthemum")){
+            icon = new ImageIcon("images/Chrysanthemum.png");
+        }else if(name.equals("Orchid")){
+            icon = new ImageIcon("images/Orchid.png");
+        }else if(name.equals("Plum")){
+            icon = new ImageIcon("images/Plum.png");
+        }else if(name.equals("Bamboo")){
+            icon = new ImageIcon("images/Bamboo.png");
+        }
+        //get the icon as an image and resize it
+        Image image = icon.getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH);
+        Image newImage = new ImageIcon(image).getImage();
+        g.drawImage(newImage, 27, 3, null);
+        setToolTipText(name);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paintFlowerTile(g);
+
+    }
+
+    public static void main(String[] args)
+    {
+        JFrame	frame = new JFrame();
+
+        frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Flower Tiles");
+
+        frame.add(new FlowerTile("Chrysanthemum"));
+        frame.add(new FlowerTile("Orchid"));
+        frame.add(new FlowerTile("Plum"));
+        frame.add(new FlowerTile("Bamboo"));
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
